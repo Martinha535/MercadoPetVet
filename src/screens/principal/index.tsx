3ebdb152753e8e379d5c85b62eabea4d8a4bebcf
Marta Santana <dra.martasantana@hotmail.com>
2022-04-11 11:24:01 -0300

    card tela inicial

--- src/screens/principal/index.tsx
+++ src/screens/principal/index.tsx
@@ -14,6 +14,8 @@ import * as Yup from "yup";
 import { Formik, validateYupSchema } from "formik";
 import { useNavigation } from "@react-navigation/native";
 import { Icon, Image } from "react-native-elements";
+import { StatusBar } from "expo-status-bar";
+import HideWithKeyboard from "react-native-hide-with-keyboard";
 
 export interface InputRoundProps {}
 
@@ -26,6 +28,8 @@ export function PrincipalScreen(props: InputRoundProps) {
 
   return (
     <>
+      <StatusBar hidden />
+
       <View
         style={{
           flexDirection: "row",
@@ -51,7 +55,7 @@ export function PrincipalScreen(props: InputRoundProps) {
       </View>
       <View
         style={{
-          marginTop: 30,
+          //marginTop: 30,
           flex: 1,
           backgroundColor: "rgb(245, 240, 240)",
           justifyContent: "space-evenly",
@@ -60,91 +64,126 @@ export function PrincipalScreen(props: InputRoundProps) {
       >
         <View
           style={{
-            flexDirection: "row",
-            width: "90%",
+            flex: 1,
+            justifyContent: "space-evenly",
           }}
         >
-          <TextInput
-            //onChangeText={handleChange("nomeCompleto")}
+          <View
             style={{
-              backgroundColor: "white",
+              flexDirection: "row",
               width: "90%",
-              height: 60,
-              padding: 10,
-              borderRadius: 15,
-              borderWidth: 2,
-              borderTopRightRadius: 0,
-              borderBottomRightRadius: 0,
-              borderRightWidth: 0,
-            }}
-            placeholder="Pesquisar"
-          />
-          <TouchableOpacity
-            style={{
-              height: 60,
-              width: 80,
-              backgroundColor: "rgb(187, 136, 59)",
-              borderWidth: 2,
-              borderTopRightRadius: 15,
-              borderBottomRightRadius: 15,
-              borderLeftWidth: 0,
-              justifyContent: "center",
-              alignItems: "center",
             }}
           >
-            <Icon color={"white"} name="search" type="ionicon"></Icon>
-          </TouchableOpacity>
+            <TextInput
+              //onChangeText={handleChange("nomeCompleto")}
+              style={{
+                backgroundColor: "white",
+                flex: 1,
+                height: 60,
+                padding: 10,
+                borderRadius: 15,
+                borderWidth: 2,
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+                borderRightWidth: 0,
+              }}
+              placeholder="Pesquisar"
+            />
+            <TouchableOpacity
+              style={{
+                height: 60,
+                width: 80,
+                backgroundColor: "rgb(187, 136, 59)",
+                borderWidth: 2,
+                borderTopRightRadius: 15,
+                borderBottomRightRadius: 15,
+                borderLeftWidth: 0,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Icon color={"white"} name="search" type="ionicon"></Icon>
+            </TouchableOpacity>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              width: "90%",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                height: 60,
+                width: "50%",
+                backgroundColor: cor,
+                borderWidth: 2,
+                borderTopLeftRadius: 15,
+                borderBottomLeftRadius: 15,
+                borderRightWidth: 0,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => {
+                setCor("rgb(187, 136, 59)");
+                setCor2("rgb(255, 255, 255)");
+                setPagina(1);
+              }}
+            >
+              <Text>Receber em casa</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                height: 60,
+                width: "50%",
+                backgroundColor: cor2,
+                borderWidth: 2,
+                borderTopRightRadius: 15,
+                borderBottomRightRadius: 15,
+                borderLeftWidth: 0,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => {
+                setCor("rgb(255, 255, 255)");
+                setCor2("rgb(187, 136, 59)");
+                setPagina(2);
+              }}
+            >
+              <Text>Retirar na loja</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            width: "90%",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              height: 60,
-              width: "50%",
-              backgroundColor: cor,
-              borderWidth: 2,
-              borderTopLeftRadius: 15,
-              borderBottomLeftRadius: 15,
-              borderRightWidth: 0,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => {
-              setCor("rgb(187, 136, 59)");
-              setCor2("rgb(255, 255, 255)");
-              setPagina(1);
-            }}
-          >
-            <Text>Receber em casa</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              height: 60,
-              width: "50%",
-              backgroundColor: cor2,
-              borderWidth: 2,
-              borderTopRightRadius: 15,
-              borderBottomRightRadius: 15,
-              borderLeftWidth: 0,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            onPress={() => {
-              setCor("rgb(255, 255, 255)");
-              setCor2("rgb(187, 136, 59)");
-              setPagina(2);
-            }}
-          >
-            <Text>Retirar na loja</Text>
-          </TouchableOpacity>
-        </View>
-        {pagina == 1 && <Text>Pagina 1</Text>}
+        
+        {pagina == 1 && (
+         // < HideWithKeyboard > 
+          <View style={{ flex:2, minWidth: "100%" }}>
+            
+            <View
+            
+              style={{
+                flex: 1,
+                backgroundColor: "#fff",
+                margin: "5%",
+                borderRadius: 10,
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 3,
+                },
+                shadowOpacity: 0.25,
+                elevation: 5,
+              }}
+              >
+            </View>
+            
+          </View>
+         // </HideWithKeyboard>
+          
+           
+        )}
 
         {pagina == 2 && <Text>Pagina 2</Text>}
+        
       </View>
     </>
   );
